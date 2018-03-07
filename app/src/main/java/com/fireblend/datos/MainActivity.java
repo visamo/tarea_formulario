a8470ab78cab642b0065e6e7a17ccd1eec154c94
Victor Hugo Salazar Montoya <visamo.cr@gmail.com>
2018-03-06 21:59:12 -0600

    Prueba de envio

--- app/src/main/java/com/fireblend/datos/MainActivity.java
+++ app/src/main/java/com/fireblend/datos/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,6 +25,10 @@ public class MainActivity extends AppCompatActivity {
     SeekBar edad;
     @BindView(R.id.textoEdad)
     TextView textoEdad;
+    @BindView(R.id.rbEl)
+    RadioButton radioButtonEl;
+    @BindView(R.id.rbElla)
+    RadioButton radioButtonElla;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -87,12 +92,31 @@ public class MainActivity extends AppCompatActivity {
             paso=false;
             return;
         }
+        String pronombre="";
+        if (radioButtonEl.isChecked()==false && radioButtonElla.isChecked()==false){
+            Toast toast =
+                    Toast.makeText(MainActivity.this, "Escoja Pronombre",
+                            Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.TOP|Gravity.LEFT,0,0);
+            toast.show();
+            paso=false;
+            return;
 
+        }
+        if (radioButtonEl.isChecked()==true) {
+            //Es Hombre
+            pronombre="El";
+        } else
+        if (radioButtonElla.isChecked()==true) {
+            //Es Mujer
+            pronombre="Ella";
+        }
         if(paso) {
             Intent intent = new Intent(MainActivity.this, SegundoActivity.class);
             intent.putExtra("nombre",nombre.getText().toString());
             intent.putExtra("edad",edad.getProgress());
             intent.putExtra("correo",correo.getText().toString());
+            intent.putExtra("pronombre",pronombre);
             startActivity(intent);
         }
     }
